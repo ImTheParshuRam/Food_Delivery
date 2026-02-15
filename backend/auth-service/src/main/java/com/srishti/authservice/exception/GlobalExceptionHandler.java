5a3648c38bb954282ea166fa74a9d556ea662169
ImTheParshuRam <sharmaparshuram2004@gmail.com>
2026-02-15 19:20:47 +0530

    auth attempt 5

---
+++ backend/auth-service/src/main/java/com/srishti/authservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.srishti.authservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        System.err.println(">>> [GlobalExceptionHandler] Caught exception: " + ex.getClass().getName());
+        System.err.println(">>> Message: " + ex.getMessage());
+        ex.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("{\"message\": \"Global Error: " + ex.getMessage() + "\", \"type\": \"" + ex.getClass().getName() + "\"}");
+    }
+}
